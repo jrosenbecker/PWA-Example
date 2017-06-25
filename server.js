b96477cc51ae05ab8f1cf48cb53c0bf0c8f75bc9
jrosenbecker <rosenbeckerjw@gmail.com>
2017-06-25 09:48:20 -0500

    Added an endpoint which returns rotating images that change every 10 seconds.

--- server.js
+++ server.js
@@ -4,6 +4,13 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
+var imageNumber = 1;
+setInterval(() => {
+    if(++imageNumber > 5) {
+        imageNumber = 1;
+    }
+}, 10 * 1000);
+
 // serve webpack build artifacts, HTML pages and dependencies
 app.use('/dist', express.static('dist'));
 app.use('/node_modules', express.static('node_modules'));
@@ -12,6 +19,10 @@ app.use(express.static('views'));
 // return the main index file
 app.get('/', (request, response) => {
     response.sendFile(path.resolve('views/index.html'));
+});
+
+app.get('/changing-image', (request, response) => {
+    response.sendFile(path.resolve(`images/Puppy-${imageNumber}.jpg`));
 });
 
 // start  the server on the proper port
